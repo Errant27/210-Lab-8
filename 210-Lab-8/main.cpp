83e7253152a8472253f9ae7c1538eb2c9780881c
Errant27 <kneil.sgo@gmail.com>
2024-09-25 16:08:43 -0700

    New commit

--- 210-Lab-8/main.cpp
+++ 210-Lab-8/main.cpp
@@ -1,14 +1,12 @@
-//
 //  main.cpp
 //  210-Lab-8
-//
-//  Created by Neil Orton on 9/25/24.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!\n";
+    
     return 0;
 }
